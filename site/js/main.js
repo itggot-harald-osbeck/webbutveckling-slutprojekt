1a6a847f0c94d1b91cb1177488890018aa5b32c8
itggot-harald-osbeck <harald.osbeck@elev.ga.ntig.se>
2019-05-10 16:52:57 +0200

    Ändringar

--- site/js/main.js
+++ site/js/main.js
@@ -2,3 +2,7 @@ function menu(){
     let thing = document.querySelector(".menu") 
     thing.classList.toggle("menuactivate")
 }
+function catmenu(){
+    let thing = document.querySelector(".categorymenu") 
+    thing.classList.toggle("catmenuactivate")
+}
