7ae348887bafcd683c5ef8dd90294df0b686e71f
itggot-harald-osbeck <harald.osbeck@elev.ga.ntig.se>
2019-05-29 11:26:25 +0200

    Transistion

--- site/js/main.js
+++ site/js/main.js
@@ -2,8 +2,12 @@ function menu(){
     let thing = document.querySelector(".menu") 
     thing.classList.toggle("menuactivate")
 }
+// function catmenu(){
+//     let thing = document.querySelector(".categorymenu") 
+//     thing.classList.toggle("catmenuactivate")
+// }
 function catmenu(){
-    let thing = document.querySelector(".categorymenu") 
+    let thing = document.querySelector(".categorycont") 
     thing.classList.toggle("catmenuactivate")
 }
 function cartback(){
